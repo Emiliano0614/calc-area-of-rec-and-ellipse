f19a6163b8c2e7c78a6ee1eca2f6d492c66d7d52
Emiliano Prado <emilianoprado12345@gmail.com>
2024-10-09 17:41:07 -0500

    Update myfunctions.h

--- myfunctions.h
+++ myfunctions.h
@@ -1,13 +1,3 @@
-/////////////////////////////////////////////////////////////////////
-//
-// Name: Emiliano Prado
-// Due date:10/9/24
-// Class: CSCI 1470.02
-// Semester: fall 2024
-// CSCI 1370 Instructor: Mr. Gustavo Dietrich
-
-//************************  Function definitions  *************************
-// Read the handout carefully for detailed description of the functions that you have to implement
 #include <fstream>
 #include <typeinfo>
 #include <iostream>
@@ -15,11 +5,9 @@
 #include <iomanip>
 using namespace std;
 
-// round_off(): Rounds the value received in the first parameter to the number of digits received in the second parameter
 double round_off(double value, int digits) {
     return round(value * static_cast<double>(digits))/10.;
 }
-// getData(): Gets two lengths from the keyboard and returns them to the caller
 double getData(double & par1,  double & par2){
     cout << "Please enter two lengths ";
     cin >> par1 >> par2;
@@ -27,8 +15,6 @@ double getData(double & par1,  double & par2){
     return par1, par2;
 }
 
-// printData(): Receives the output file, base of the rectangle, height of the rectangle, area of the rectangle,
-// radius a of the ellipse, radius b of the ellipse, and area of the ellipse and prints the output to the file
 void printData(ofstream & oFile,double b,double h,double ar,double ra,double rb,double ae){
     cout << fixed << setprecision(2);
     oFile << fixed << setprecision(2);
@@ -41,13 +27,11 @@ void printData(ofstream & oFile,double b,double h,double ar,double ra,double rb,
 }
 
 
-// area_rectangle(): Calculates the area of the rectangle and returns it rounded to 1 decimal digit to the caller
 double area_rectangle(double b, double h ){
     double arear = b * h;
     return round_off (arear, 10.);
 }
 
-// area_ellipse(): Calculates the area of the area_ellipse and returns it rounded to 1 decimal digit to the caller
 double area_ellipse(double ra, double  rb ){
 
     double areae = PI * ra * rb;
